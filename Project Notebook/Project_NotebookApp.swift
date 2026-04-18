7a9f5f750d33138fad7ff58c558cff97e6e3cdf5
hazani <ardayigit0518@icloud.com>
2026-04-18 21:29:55 +0300

    Initial Commit

---
+++ Project Notebook/Project_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_NotebookApp.swift
+//  Project Notebook
+//
+//  Created by Arda Yiğit on 18.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Project_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
